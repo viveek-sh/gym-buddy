45e8556b826602ba44cf3de8a85a3cca0a1c38ea
vivek <get.viveksahu@gmail.com>
2025-11-10 12:28:02 +0530

    modify deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   distDir: "dist",
+  basePath: isProd ? "/gym-buddy" : "",
+  assetPrefix: isProd ? "/gym-buddy/" : "",
+  images: {
+    unoptimized: true, // Needed for static export
+  },
 };
 
 export default nextConfig;
