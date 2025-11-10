7c21dae0d53c3b63752eef86353c8adb5b79d0a2
vivek <get.viveksahu@gmail.com>
2025-11-10 12:08:20 +0530

    build fail handled

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   /* config options here */
-  async headers() {
-    return [
-      {
-        // Apply CORS to all routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  output: "export",
+  distDir: "dist",
 };
 
 export default nextConfig;
